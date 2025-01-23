f751bc9c35be74697ec4a3d4f4b5931297a853f4
Akshay-Syscort <akshay.shaharwale@syscort.com>
2025-01-23 12:14:46 +0530

    doctype 2 added

---
+++ akshay_custom_app/akshay_custom_app/doctype/custom_doctype_2/custom_doctype_2.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, akshay and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("custom doctype 2", {
+// 	refresh(frm) {
+
+// 	},
+// });
